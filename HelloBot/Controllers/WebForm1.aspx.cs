e7acd4f10f33ec77dc58a9f8c46c9e2ca2c41a0c
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-03-15 17:45:17 +0200

    add a form showing users table

---
+++ HelloBot/Controllers/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloBot.Controllers
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["daphne"].ConnectionString;
+                conn.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Users", conn);
+                resultStr.Text = @"Connected...<br>";
+                resultStr.Text += "ID\tTeamID\tUsername\tFullName" +
+                                  "<br>";
+
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    
+                    while (reader.Read())
+                    {
+                        resultStr.Text += $"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}<br>";
+                    }
+                }
+                // using the code here...
+            }
+            
+        }
+    }
+}
