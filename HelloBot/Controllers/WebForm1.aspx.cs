ec65be3e0bf174290326a5d957bb3e84e9c1ff75
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-03-15 22:16:23 +0200

    generated model and context

--- HelloBot/Controllers/WebForm1.aspx.cs
+++ HelloBot/Controllers/WebForm1.aspx.cs
@@ -14,27 +14,10 @@ namespace HelloBot.Controllers
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["daphne"].ConnectionString;
-                conn.Open();
-                SqlCommand command = new SqlCommand("SELECT * FROM Users", conn);
-                resultStr.Text = @"Connected...<br>";
-                resultStr.Text += "ID\tTeamID\tUsername\tFullName" +
-                                  "<br>";
-
-
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    
-                    while (reader.Read())
-                    {
-                        resultStr.Text += $"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}<br>";
-                    }
-                }
+           
+            
                 // using the code here...
-            }
+            
             
         }
     }
