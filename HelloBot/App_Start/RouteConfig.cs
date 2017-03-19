c6903dc0154867a36e719638c364be98e5851cf8
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-03-19 17:36:32 +0200

    added link to admin panel to default.htm removed attachment mode of sending picture, now it just sends link

--- HelloBot/App_Start/RouteConfig.cs
+++ HelloBot/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HelloBot
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { action = "Forms/Index.aspx", id = UrlParameter.Optional }
             );
         }
     }
--- HelloBot/Controllers/MessagesController.cs
+++ HelloBot/Controllers/MessagesController.cs
@@ -31,18 +31,13 @@ namespace HelloBot
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
+                var now = DateTime.Now;
 
                 // return our reply to the user
                 Activity reply = activity.CreateReply($"Hello, IoT!" +
-                                                      $"I am Daphne and I will collect your statuses someday. But for now take this kitty.");
+                                                      $"I am Daphne and I will collect your statuses someday. But for now take this kitty."
+                                                      + $"http://thecatapi.com/api/images/get?format=src&type=png&timestamp=" + $"{now}");
 
-                reply.Attachments = new List<Attachment>();  //****** INIT
-                var ts = DateTime.Now;
-                reply.Attachments.Add(new Attachment()
-                {
-                    ContentUrl = $"http://thecatapi.com/api/images/get?format=src&type=png&timestamp=" +$"{ts}",
-                    ContentType = "image/png"
-                });
 
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
