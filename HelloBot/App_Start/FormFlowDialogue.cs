a22595f72306d639021bf14d9cf00646ef33710a
GreedyGoblim <goblimgus@gmail.com>
2017-03-30 15:15:39 +0300

    Dialogue through FormFlow(not working)

---
+++ HelloBot/App_Start/FormFlowDialogue.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+
+namespace HelloBot
+{
+    public enum StatusOptions
+    {
+        Did, WillDo, Problems
+    };
+    //public String DidStatus {  };
+    //public String WillDoStatus {  };
+    //public String ProblemsStatus {  };
+
+    [Serializable]
+    public class CompleteStatus
+    {
+        public StatusOptions? StatusOptions;
+        //public DidStatus? Done;
+        //public WillDoStatus? WillDo;
+        //public ProblemsStatus? Problems;
+
+        public static IForm<CompleteStatus> BuildForm()
+        {
+            return new FormBuilder<CompleteStatus>()
+                    .Message("Welcome to the simple Status writing Daphne bot!")
+                    .Build();
+        }
+    };
+}
--- HelloBot/Controllers/MessagesController.cs
+++ HelloBot/Controllers/MessagesController.cs
@@ -6,73 +6,98 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.UI.WebControls;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
 
 
 namespace HelloBot
 {
-
-
-
-    [BotAuthentication]
-    public class MessagesController : ApiController
+    internal static IDialog<CompleteStatus> MakeRootDialog()
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
-        /// 
-        /// 
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        return Chain.From(() => FormDialog.FromForm(CompleteStatus.BuildForm));
+    }
+    [ResponseType(typeof(void))]
+    public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+    {
+        if (activity != null)
         {
-            if (activity.Type == ActivityTypes.Message)
+            // one of these will have an interface and process it
+            switch (activity.GetActivityType())
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-
-                var now = DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss");
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"Hello, IoT! " +
-                                                      $"I am Daphne and I will collect your statuses someday. But for now take this kitty. "
-                                                      + $"http://thecatapi.com/api/images/get?format=src&type=png&timestamp=" + $"{now}");
-
-
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                case ActivityTypes.Message:
+                    await Conversation.SendAsync(activity, MakeRootDialog);
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                case ActivityTypes.ContactRelationUpdate:
+                case ActivityTypes.Typing:
+                case ActivityTypes.DeleteUserData:
+                default:
+                    Trace.TraceError($"Unknown activity type ignored: {activity.GetActivityType()}");
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
-        }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
         }
     }
-}
+
+
+                    //[BotAuthentication]
+                    //public class MessagesController : ApiController
+                    //{
+                    //    /// <summary>
+                    //    /// POST: api/Messages
+                    //    /// Receive a message from a user and reply to it
+                    //    /// </summary>
+                    //    /// 
+                    //    /// 
+                    //    public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+                    //    {
+                    //        if (activity.Type == ActivityTypes.Message)
+                    //        {
+                    //            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                    //            var now = DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss");
+
+                    //            // return our reply to the user
+                    //            Activity reply = activity.CreateReply($"Hello, IoT! " +
+                    //                                                  $"I am Daphne and I will collect your statuses someday. But for now take this kitty. "
+                    //                                                  + $"http://thecatapi.com/api/images/get?format=src&type=png&timestamp=" + $"{now}");
+
+
+                    //            await connector.Conversations.ReplyToActivityAsync(reply);
+                    //        }
+                    //        else
+                    //        {
+                    //            HandleSystemMessage(activity);
+                    //        }
+                    //        var response = Request.CreateResponse(HttpStatusCode.OK);
+                    //        return response;
+                    //    }
+
+                    //    private Activity HandleSystemMessage(Activity message)
+                    //    {
+                    //        if (message.Type == ActivityTypes.DeleteUserData)
+                    //        {
+                    //            // Implement user deletion here
+                    //            // If we handle user deletion, return a real message
+                    //        }
+                    //        else if (message.Type == ActivityTypes.ConversationUpdate)
+                    //        {
+                    //            // Handle conversation state changes, like members being added and removed
+                    //            // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                    //            // Not available in all channels
+                    //        }
+                    //        else if (message.Type == ActivityTypes.ContactRelationUpdate)
+                    //        {
+                    //            // Handle add/remove from contact lists
+                    //            // Activity.From + Activity.Action represent what happened
+                    //        }
+                    //        else if (message.Type == ActivityTypes.Typing)
+                    //        {
+                    //            // Handle knowing tha the user is typing
+                    //        }
+                    //        else if (message.Type == ActivityTypes.Ping)
+                    //        {
+                    //        }
+
+                    //        return null;
+                    //    }
+                    //}
+            }
