97f1d9c1a15c77ae4c55da12125f3dd5a031de30
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-04-13 01:50:15 +0200

    naive hardcode implementation of bot asking 3 questions and saving answers to database

--- HelloBot/Controllers/MessagesController.cs
+++ HelloBot/Controllers/MessagesController.cs
@@ -10,41 +10,47 @@ using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Web.Http.Description;
 using System.Linq;
+using SlackAPI;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+
 namespace HelloBot
     {
 
 
 
-        [Serializable]
-        public class DialogAnswer
-        {
-        // TODO:
-        // write the same IForm<CompleteStatus> BuildForm() for entity class which i will get from master 
-        // and fill fields from entity(or some model class* optionally) - that`s getting questions from database
-        // and save changes in database context in saveState function
-        //public List<string> l = new List<string>(); // TO DO from GetQuestions
-        public string First { get; set; }
-        public string Second { get; set; }
-        public List<Question> Questions { get 
-        {
-                using (var ctx = new DaphneBotEntities())
-                {
-                    return (from temp in ctx.Questions select temp).ToList();
-                }
-            }
-        }
+    [Serializable]
+    public class DialogAnswer
+    {
+        [Prompt("What have you already done?")]
+        public string Done { get; set; }
+        [Prompt("What will you do?")]
+        public string WillDo { get; set; }
+        [Prompt("What problems do you have?")]
+        public string Problems { get; set; }
+
         public static IForm<DialogAnswer> BuildForm()
         {
+            async Task<bool> SaveToDb(IDialogContext ctx, DialogAnswer state)
+            {
+                using (DaphneBotEntities db = new DaphneBotEntities())
+                {
+                    // if status has no answers, then do 
+                    db.QAs.Add(new QA() { answer = state.Done, questionId = 1, whenCollected = DateTime.Now, statusId = 1/* to define to what status is this answer*/ });
+                    db.QAs.Add(new QA() { answer = state.WillDo, questionId = 2, whenCollected = DateTime.Now, statusId = 1/* to define to what status is this answer*/ });
+                    db.QAs.Add(new QA() { answer = state.Problems, questionId = 3, whenCollected = DateTime.Now, statusId = 1/* to define to what status is this answer*/ });
+                    db.SaveChanges();
+                    
+                    return true;
+                    // else return false;
+                }
+            }
             OnCompletionAsyncDelegate<DialogAnswer> saveState = async (context, state) =>
             {
-            // to implement saving state to database:
-            // add fields (state) to db context and save it
-            
-            await context.PostAsync("some message");
-            
-            // throw notimplementedexception
+                if (await SaveToDb(context, state))
+                    await context.PostAsync("Your status was saved");
+                else await context.PostAsync("Something went wrong and your status wasn't saved :(");
+            };
 
-        };
             return new FormBuilder<DialogAnswer>()
                     .Message("Welcome to the simple Status writing Daphne bot!")
                     .OnCompletion(saveState)
@@ -65,7 +71,7 @@ namespace HelloBot
                 {
                     if (activity.Type == ActivityTypes.Message)
                     {
-                        await Conversation.SendAsync(activity, MakeRoot);
+                        await Microsoft.Bot.Builder.Dialogs.Conversation.SendAsync(activity, MakeRoot);
                     }
                     else
                     {
